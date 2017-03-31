06ab3441f3213e3a3023ee7b1a9d31f336705655
LuisGLunaB <luis@mkti.mx>
2017-03-30 22:10:15 -0600

    First Commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+	$titulo = "Framework";  
+	$descripcion = "Descripción";
+	$men = "";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html lang="es-MX" xml:lang="es" xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/plantilla.dwt.php" codeOutsideHTMLIsLocked="false" -->
+
+<head>
+<?php include_once("./blocks/metas.php"); include_once("./blocks/scripts.php");?>
+<!-- InstanceBeginEditable name="head" -->
+    <style type="text/css">
+	
+    </style>
+    <!-- InstanceEndEditable -->
+</head>
+
+<body onresize="spanner();" onload="spanner();">
+<?php include_once("./blocks/header.php");?>
+    <!-- InstanceBeginEditable name="PreContainer" -->
+    PreContainer
+	<!-- InstanceEndEditable -->
+    <div class="container">
+	<!-- InstanceBeginEditable name="Container" -->
+		Cuerpo
+        <!-- InstanceEndEditable -->
+    </div>
+<?php include_once("./blocks/footer.php");?>
+</body>
+
+<?php include_once("./blocks/scripts2.php");?>
+<!-- InstanceEnd --></html>
